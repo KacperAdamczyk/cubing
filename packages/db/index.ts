92984a79304fc19b76cccf48c67b71e59eedfc9c
Kacper Adamczyk <kacper.k.adamczyk@gmail.com>
2024-01-09 00:06:35 +0100

    Test

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -3,17 +3,35 @@ import {
   drizzle,
 } from "drizzle-orm/better-sqlite3";
 import Database, { type Database as DatabaseType } from "better-sqlite3";
+import { globSync } from "glob";
 import * as schema from "./schema";
+import fs from "node:fs";
 
 import packageJson from "./package.json";
 
 const fileName = "database.db";
-const dbPath = `./node_modules/${packageJson.name}/${fileName}`;
+const dbPath = `**/node_modules/${packageJson.name}/${fileName}`;
+console.log(process.cwd());
+fs.readdirSync("./").forEach((file) => {
+  console.log(file);
+});
+const matches = globSync(dbPath);
+
+if (matches.length === 0) {
+  throw new Error(`Could not find database file matching: ${dbPath}`);
+} else if (matches.length > 1) {
+  throw new Error(
+    `Found multiple database files: ${matches}, matching: ${dbPath}`
+  );
+}
+
+const dbFile = matches[0]!;
+
 let database: BetterSQLite3Database<typeof schema> | undefined;
 export let sqlite: DatabaseType | undefined;
 export const db = () => {
   if (!database) {
-    sqlite = new Database(dbPath, { readonly: true });
+    sqlite = new Database(dbFile, { readonly: true });
     database = drizzle(sqlite, {
       schema,
       logger: true,
