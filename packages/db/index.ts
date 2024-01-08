1135f60bf12ec3a854d1845211945c4e39c21f4c
Kacper Adamczyk <kacper.k.adamczyk@gmail.com>
2024-01-09 00:45:50 +0100

    Fix deployment to GP (#6)
    * Test
    
    * Update lock file
    
    * Test
    
    * Resolve db path with globe
    
    * Improve

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -3,17 +3,39 @@ import {
   drizzle,
 } from "drizzle-orm/better-sqlite3";
 import Database, { type Database as DatabaseType } from "better-sqlite3";
+import { globSync } from "glob";
 import * as schema from "./schema";
+import fs from "node:fs";
 
 import packageJson from "./package.json";
 
 const fileName = "database.db";
-const dbPath = `./node_modules/${packageJson.name}/${fileName}`;
+const rootsToCheck = ["./", "../../"] as const;
+const dbPath = `**/node_modules/${packageJson.name}/${fileName}`;
+let foundPath: string | undefined;
+
+for (const root of rootsToCheck) {
+  const path = globSync(`${root}${dbPath}`).at(0);
+
+  if (path) {
+    foundPath = path;
+    break;
+  }
+}
+
+if (!foundPath) {
+  throw new Error(`Could not find database file matching: ${dbPath}`);
+}
+
 let database: BetterSQLite3Database<typeof schema> | undefined;
 export let sqlite: DatabaseType | undefined;
 export const db = () => {
+  if (!foundPath) {
+    throw new Error(`Could not find database file matching: ${dbPath}`);
+  }
+
   if (!database) {
-    sqlite = new Database(dbPath, { readonly: true });
+    sqlite = new Database(foundPath, { readonly: true });
     database = drizzle(sqlite, {
       schema,
       logger: true,
