1b193fb0397d11db97c965bd2fbb151f5b8f1c67
Kacper Adamczyk <kacper.k.adamczyk@gmail.com>
2024-03-09 00:33:33 +0100

    F2L face

--- apps/dashboard/components/F2L/F2L.tsx
+++ apps/dashboard/components/F2L/F2L.tsx
@@ -1,36 +1,36 @@
-import {Face} from "@/components/Face";
-import {Colors, algorithmToFaces} from "@repo/cube";
-import type {FC} from "react";
+import { Face } from "@/components/Face";
+import { Colors, algorithmToFaces } from "@repo/cube";
+import type { FC } from "react";
 
 interface Props {
-    algorithm: string;
+  algorithm: string;
 }
 
-export const F2L: FC<Props> = ({algorithm}) => {
-    const faces = algorithmToFaces({
-        algorithm,
-        orientation: {U: Colors.Y, F: Colors.R},
-    });
+export const F2L: FC<Props> = ({ algorithm }) => {
+  const faces = algorithmToFaces({
+    algorithm,
+    orientation: { U: Colors.Y, F: Colors.R },
+  });
 
-    return (
-        <div className="size-40 [perspective:800px]">
-            <div className="relative size-full origin-center [transform-style:preserve-3d]">
-                <Face
-                    className="absolute size-[50%] [transform:scale(0.5)_rotateX(45deg)_translateZ(200px)]"
-                    face={faces.U}
-                    maskedColors={[]}
-                />
-                <Face
-                    className="absolute size-[50%] [transform:scale(0.5)_translateZ(200px)]"
-                    face={faces.F}
-                    maskedColors={[]}
-                />
-                <Face
-                    className="absolute size-[50%] [transform:scale(0.5)_rotateY(45deg)_translateZ(200px)]"
-                    face={faces.R}
-                    maskedColors={[]}
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div className="aspect-square size-80 [perspective:800px]">
+      <div className="relative size-full origin-center [transform-style:preserve-3d]">
+        <Face
+          className="absolute bottom-[25px] left-[25px] !size-[50%] [transform:rotateX(45deg)_rotateX(-90deg)_translateX(50%)_rotateX(90deg)]"
+          face={faces.U}
+          maskedColors={[]}
+        />
+        <Face
+          className="absolute bottom-[25px] left-[25px] !size-[50%] [transform:translateZ(180px)]"
+          face={faces.F}
+          maskedColors={[]}
+        />
+        <Face
+          className="absolute bottom-[25px] left-[25px] !size-[50%] [transform:rotateY(45deg)_translateZ(180px)]"
+          face={faces.R}
+          maskedColors={[]}
+        />
+      </div>
+    </div>
+  );
 };
