be030c34aab0a65cf3f08213f8d0907d6fce5572
Kacper Adamczyk <kacper.k.adamczyk@gmail.com>
2024-06-04 22:18:22 +0200

    pnpm

--- apps/dashboard/next.config.js
+++ apps/dashboard/next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   output: "export",
   basePath: process.env.BASE_PATH ?? "/__dev__",
+    experimental: {
+        reactCompiler: true,
+        ppr: true,
+    },
 };
 
 export default nextConfig;
