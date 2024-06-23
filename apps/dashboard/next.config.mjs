cb223b6ba91af21980dec316582d4e6f887d2f8e
Kacper Adamczyk <kacper.k.adamczyk@gmail.com>
2024-06-23 16:03:09 +0200

    Prettier

--- apps/dashboard/next.config.mjs
+++ apps/dashboard/next.config.mjs
@@ -1,9 +1,9 @@
 let basePath;
 if (process.env.NODE_ENV === "development") {
-    basePath = process.env.BASE_PATH ?? "/__dev__";
+  basePath = process.env.BASE_PATH ?? "/__dev__";
 }
 if (process.env.NODE_ENV === "production") {
-    basePath = process.env.BASE_PATH;
+  basePath = process.env.BASE_PATH;
 }
 
 console.info(`Base path: ${basePath}`);
