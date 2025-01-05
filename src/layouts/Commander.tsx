caf0fa32fc88e0493c372b5cbe7dfd44b4bd2742
Kacper Adamczyk <kacper.k.adamczyk@gmail.com>
2025-01-05 19:51:08 +0100

    feat: enhance keyboard shortcuts in Commander component for toggling dialog

--- src/layouts/Commander.tsx
+++ src/layouts/Commander.tsx
@@ -15,7 +15,7 @@ export const Commander: FC = () => {
 
   useEffect(() => {
     const down = (e: KeyboardEvent) => {
-      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+      if ((e.key === "k" && (e.metaKey || e.ctrlKey)) || e.key === "/") {
         e.preventDefault();
         setOpen((open) => !open);
       }
