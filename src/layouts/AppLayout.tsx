0f4da14742b74b190d3a1648400608473a7945fc
Kacper Adamczyk <kacper.k.adamczyk@gmail.com>
2025-01-13 19:52:12 +0100

    Populate commander

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -7,40 +7,40 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar";
 import { Breadcrumbs, type BreadcrumbEntry } from "@/layouts/Breadcrumbs";
-import { Commander } from "@/layouts/Commander";
+import { Commander, type Case } from "@/layouts/Commander";
 
 interface AppLayoutProps {
   sets: Set[];
   pathname: string;
   breadcrumbs: BreadcrumbEntry[];
+  cases: Case[];
 }
 
 export const AppLayout: FC<PropsWithChildren<AppLayoutProps>> = ({
   sets,
   pathname,
   breadcrumbs,
+  cases,
   children,
-}) => {
-  return (
-    <SidebarProvider>
-      <AppSidebar sets={sets} pathname={pathname} />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b">
-          <div className="flex items-center gap-2 px-3">
-            <SidebarTrigger />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumbs breadcrumbs={breadcrumbs} />
-            <div>
-              <Commander />
-            </div>
-          </div>
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4">
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
-            {children}
+}) => (
+  <SidebarProvider>
+    <AppSidebar sets={sets} pathname={pathname} />
+    <SidebarInset>
+      <header className="flex h-16 shrink-0 items-center gap-2 border-b">
+        <div className="flex flex-1 items-center gap-2 px-3">
+          <SidebarTrigger />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Breadcrumbs breadcrumbs={breadcrumbs} />
+          <div className="ml-auto">
+            <Commander cases={cases} />
           </div>
         </div>
-      </SidebarInset>
-    </SidebarProvider>
-  );
-};
+      </header>
+      <div className="flex flex-1 flex-col gap-4 p-4">
+        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
+          {children}
+        </div>
+      </div>
+    </SidebarInset>
+  </SidebarProvider>
+);
--- src/queries/getAllCases.ts
+++ src/queries/getAllCases.ts
@@ -4,7 +4,15 @@ import { getCollection } from "astro:content";
 export const getAllCases = async () => {
   const sets = await getCollection("sets");
 
-  const cases = await Promise.all(sets.map(({ id }) => getSetCases(id)));
+  const cases = await Promise.all(
+    sets.map(async (set) => {
+      const cases = await getSetCases(set.id);
 
-  return cases.flat();
+      return [set, cases] as const;
+    }),
+  );
+
+  return cases.flatMap(([set, cases]) =>
+    cases.map((c) => ({ ...c, set: set.data })),
+  );
 };
