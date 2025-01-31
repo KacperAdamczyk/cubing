88d8cf21c9d3a953482288823ba9b372bd5a7b61
Kacper Adamczyk <kacper.k.adamczyk@gmail.com>
2025-01-31 10:55:13 +0100

    Astro-5.2 (#481)
    * Update packages
    
    * Migrate tailwind
    
    * fix(workflow): update pnpm setup to use the latest version
    
    * Format

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -37,7 +37,7 @@ export const AppLayout: FC<PropsWithChildren<AppLayoutProps>> = ({
         </div>
       </header>
       <div className="flex flex-1 flex-col gap-4 p-4">
-        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
+        <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min">
           {children}
         </div>
       </div>
