5962397e43e90fce058d5b260dec5863a978d0e6
Kacper Adamczyk <kacper.k.adamczyk@gmail.com>
2025-01-31 10:52:42 +0100

    Format

--- src/layouts/SearchForm.tsx
+++ src/layouts/SearchForm.tsx
@@ -26,7 +26,7 @@ export const SearchForm: FC<SearchFormProps> = ({ query, setQuery }) => {
           value={query}
           onChange={(event) => setQuery(event.target.value)}
         />
-        <Search className="pointer-events-none absolute left-2 top-1/2 size-4 -translate-y-1/2 select-none opacity-50" />
+        <Search className="pointer-events-none absolute top-1/2 left-2 size-4 -translate-y-1/2 opacity-50 select-none" />
       </SidebarGroupContent>
     </SidebarGroup>
   );
