16a13124993af80d1510682f8674fa96f693d6c7
Kacper Adamczyk <kacper@admck.com>
2025-12-10 21:55:07 +0100

    Migrate project to Bun and update schema (#838)
    * Update schema
    
    * Migrate to bun
    
    * cleanup

--- packages/db/schema.ts
+++ packages/db/schema.ts
@@ -1,76 +1,76 @@
-import {relations} from "drizzle-orm";
+import { relations } from "drizzle-orm";
 import {
-    type AnySQLiteColumn,
-    sqliteTable,
-    text,
+  type AnySQLiteColumn,
+  sqliteTable,
+  text,
 } from "drizzle-orm/sqlite-core";
 
 const viewType = ["F2L", "OLL", "PLL"] as const;
 
-export const cubes = sqliteTable("cubes", {
-    id: text().primaryKey(),
-    name: text().notNull().unique(),
+export const cube = sqliteTable("cube", {
+  id: text().primaryKey(),
+  name: text().notNull().unique(),
 });
 
-export const cubesRelations = relations(cubes, ({many}) => ({
-    sets: many(sets),
+export const cubeRelations = relations(cube, ({ many }) => ({
+  sets: many(set),
 }));
 
-export const sets = sqliteTable("sets", {
-    id: text().primaryKey(),
-    name: text().notNull().unique(),
-    previewAlgorithm: text().notNull(),
-    cubeId: text()
-        .notNull()
-        .references(() => cubes.id),
-    viewType: text({enum: viewType}).notNull(),
+export const set = sqliteTable("set", {
+  id: text().primaryKey(),
+  name: text().notNull().unique(),
+  previewAlgorithm: text().notNull(),
+  cubeId: text()
+    .notNull()
+    .references(() => cube.id),
+  viewType: text({ enum: viewType }).notNull(),
 });
 
-export const setsRelations = relations(sets, ({one, many}) => ({
-    cube: one(cubes, {fields: [sets.cubeId], references: [cubes.id]}),
-    subsets: many(subsets),
+export const setRelations = relations(set, ({ one, many }) => ({
+  cube: one(cube, { fields: [set.cubeId], references: [cube.id] }),
+  subsets: many(subset),
 }));
 
-export const subsets = sqliteTable("subsets", {
-    id: text().primaryKey(),
-    name: text().notNull().unique(),
-    previewAlgorithm: text().notNull(),
-    setId: text()
-        .notNull()
-        .references(() => sets.id),
+export const subset = sqliteTable("subset", {
+  id: text().primaryKey(),
+  name: text().notNull().unique(),
+  previewAlgorithm: text().notNull(),
+  setId: text()
+    .notNull()
+    .references(() => set.id),
 });
 
-export const subsetsRelations = relations(subsets, ({one, many}) => ({
-    set: one(sets, {fields: [subsets.setId], references: [sets.id]}),
-    cases: many(cases),
+export const subsetRelations = relations(subset, ({ one, many }) => ({
+  set: one(set, { fields: [subset.setId], references: [set.id] }),
+  cases: many(_case),
 }));
 
-export const cases = sqliteTable("cases", {
-    id: text().primaryKey(),
-    name: text().notNull().unique(),
-    setup: text().notNull(),
-    subsetId: text()
-        .notNull()
-        .references(() => subsets.id),
-    defaultAlgorithmId: text().references((): AnySQLiteColumn => algorithms.id),
+export const _case = sqliteTable("case", {
+  id: text().primaryKey(),
+  name: text().notNull().unique(),
+  setup: text().notNull(),
+  subsetId: text()
+    .notNull()
+    .references(() => subset.id),
+  defaultAlgorithmId: text().references((): AnySQLiteColumn => algorithm.id),
 });
 
-export const casesRelations = relations(cases, ({one, many}) => ({
-    subset: one(subsets, {fields: [cases.subsetId], references: [subsets.id]}),
-    algorithms: many(algorithms),
+export const caseRelations = relations(_case, ({ one, many }) => ({
+  subset: one(subset, { fields: [_case.subsetId], references: [subset.id] }),
+  algorithms: many(algorithm),
 }));
 
-export const algorithms = sqliteTable("algorithms", {
-    id: text().primaryKey(),
-    name: text().notNull().unique(),
-    caseId: text()
-        .notNull()
-        .references(() => cases.id),
-    rotations: text().notNull().unique(),
-    mnemonics: text(),
-    description: text(),
+export const algorithm = sqliteTable("algorithm", {
+  id: text().primaryKey(),
+  name: text().notNull().unique(),
+  caseId: text()
+    .notNull()
+    .references(() => _case.id),
+  rotations: text().notNull().unique(),
+  mnemonics: text(),
+  description: text(),
 });
 
-export const algorithmsRelations = relations(algorithms, ({one}) => ({
-    case: one(cases, {fields: [algorithms.caseId], references: [cases.id]}),
+export const algorithmRelations = relations(algorithm, ({ one }) => ({
+  case: one(_case, { fields: [algorithm.caseId], references: [_case.id] }),
 }));
