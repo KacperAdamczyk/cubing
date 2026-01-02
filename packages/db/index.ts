5bcab79d5ed5230b3277e49d3b017a79c5679cf5
Kacper Adamczyk <kacper@admck.com>
2026-01-02 13:51:35 +0100

    fix: clean up formatting in db index file

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -2,11 +2,10 @@ import { drizzle } from "drizzle-orm/bun-sqlite";
 // @ts-expect-error
 import database from "./db.sqlite" with { embed: "true", type: "sqlite" };
 import { relations } from "./relations";
-import * as schema from "./schema";
 
 export const db = drizzle(database, {
-	relations,
-	casing: "snake_case",
+  relations,
+  casing: "snake_case",
 });
 
 export * from "./schema";
